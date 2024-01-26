d04c90016d3c5de8484826ff295d9ef01ebe283c
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2024-01-27 00:52:55 +0300

    Config klasoru ve api.js dosyası oluşturuldu

---
+++ frontend/src/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+    baseUrl: 'http://localhost:8080',
+    //appName: 'My Awesome App',
+    //themeColor: '#3498db',
+    // Diğer konfigürasyon bilgileri...
+  };
+
+  export default config;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@/styles/main.css';
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/main.css";
+import router from "./router";
+import store from "./store";
+import api from "@/services/api.js"
 
-createApp(App).use(store).use(router)
-.mount('#app');
+// import axios from "axios";
+// import config from "@/config";
+
+// axios.create({
+//   baseURL: config.baseUrl,
+// });
+
+createApp(App).use(store).use(router).use(api).mount("#app");
