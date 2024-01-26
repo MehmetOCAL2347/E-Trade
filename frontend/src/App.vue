e2718b921eb2776127f9694a08369f67f8549c1d
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2024-01-27 00:28:17 +0300

    Router, vuex, axios eklendi

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,30 @@
 <template>
-  <the-header />
-  <the-main />
-  <the-footer />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import TheFooter from "./components/common/TheFooter.vue";
-import TheHeader from "./components/common/TheHeader.vue";
-import TheMain from "./components/common/TheMain.vue";
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: {
-    TheHeader,
-    TheMain,
-    TheFooter,
-  },
-};
-</script>
-<style></style>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/main.css';
+import router from './router'
+import store from './store'
 
-createApp(App)
+createApp(App).use(store).use(router)
 .mount('#app');
